4f4339be994734b68f287456f676da7cfdaaf22e
fierroargento <nauticadelplata@yahoo.com.ar>
2026-06-12 15:08:04 -0300

    Limpiar codigo muerto en cierre de reclamo ML

--- app.py
+++ app.py
@@ -12322,10 +12322,7 @@ def cerrar_reclamo_ml_devolucion(id):
         error=""
     )
 
-    pedido.estado = "Finalizado"
-    db.session.commit()
 
-    return redirect(url_for("detalle_pedido", id=pedido.id))
     
 @app.route("/pedido/<int:id>/revisar-reclamo", methods=["POST"])
 @login_required
